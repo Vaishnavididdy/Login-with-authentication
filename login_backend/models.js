6e785dfe3074f0f9bb2807e1163e362aa0a4ef7e
Vaishnavididdy <yshnavi7115@gmail.com>
2025-01-28 11:46:59 +0530

    Added backend code

---
+++ login_backend/models.js
@@ -0,0 +1,11 @@
+// models/Item.js 
+const mongoose = require('mongoose');
+
+const ItemSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Item', ItemSchema);
